9d661f10a18fbaee79027b43eda09c4daa65b4ae
stampaaaron <aaron@stampa.ch>
2023-02-20 16:05:25 +0100

    fix: initializing totals

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -3,14 +3,14 @@ import 'player.dart';
 class Game {
   int maxPoints = 100;
   List<Player> players = [];
-  late final List<Map<Player, int?>> rounds;
+  late final List<Map<Player, int?>> rounds = [];
 
-  Map<Player, int?> get totals => rounds.reduce((prev, cur) =>
-      prev.map((key, value) => MapEntry(key, (value ?? 0) + (cur[key] ?? 0))));
+  Map<Player, int?> get totals => rounds.isNotEmpty
+      ? rounds.reduce((prev, cur) => prev
+          .map((key, value) => MapEntry(key, (value ?? 0) + (cur[key] ?? 0))))
+      : Map.fromEntries(players.map((player) => (MapEntry(player, 0))));
 
-  Game(this.players) {
-    rounds = [Game.setupRoundEntry(players)];
-  }
+  Game(this.players);
 
   static setupRoundEntry(List<Player> players) =>
       Map.fromEntries(players.map((player) => (MapEntry(player, null))));
