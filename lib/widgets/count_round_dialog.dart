0bcbedfe1a41b0c3da0b9c4fb8dbec5c60743cae
stampaaaron <aaron@stampa.ch>
2023-02-21 11:45:21 +0100

    fix: dialog overflow issues, add stylings

--- lib/widgets/count_round_dialog.dart
+++ lib/widgets/count_round_dialog.dart
@@ -24,35 +24,43 @@ class _CountRoundDialogState extends State<CountRoundDialog> {
 
   @override
   build(context) {
+    final theme = Theme.of(context);
+
     return AlertDialog(
       title: const Text("Runde zählen"),
-      content: Form(
-        key: formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("$usedPoints von 157 Punkten sind vergeben."),
-            ...widget.players
-                .map((player) => TextFormField(
-                      autofocus: player == widget.players.first,
-                      decoration: InputDecoration(labelText: player.username),
-                      keyboardType: TextInputType.number,
-                      textInputAction: TextInputAction.next,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                      ],
-                      validator: notEmptyValidator,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onChanged: (value) => setState(
-                          () => points[player] = int.tryParse(value) ?? 0),
-                      onSaved: (value) {
-                        if (value == null) return;
-                        setState(
-                            () => points[player] = int.tryParse(value) ?? 0);
-                      },
-                    ))
-                .toList()
-          ],
+      content: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "$usedPoints von 157",
+                style: theme.textTheme.labelLarge,
+              ),
+              ...widget.players
+                  .map((player) => TextFormField(
+                        autofocus: player == widget.players.first,
+                        decoration: InputDecoration(labelText: player.username),
+                        keyboardType: TextInputType.number,
+                        textInputAction: TextInputAction.next,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        validator: notEmptyValidator,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        onChanged: (value) => setState(
+                            () => points[player] = int.tryParse(value) ?? 0),
+                        onSaved: (value) {
+                          if (value == null) return;
+                          setState(
+                              () => points[player] = int.tryParse(value) ?? 0);
+                        },
+                      ))
+                  .toList()
+            ],
+          ),
         ),
       ),
       actions: [
