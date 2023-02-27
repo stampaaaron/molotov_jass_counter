e0efb9292a577f38a24123c65dd076507255677b
stampaaaron <aaron@stampa.ch>
2023-02-27 09:30:42 +0100

    feat: introduce continue last game button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,19 +19,29 @@ class HomeScreen extends StatelessWidget {
         )),
         padding: EdgeInsets.all(8),
         child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50),
+          child: Wrap(
+            direction: Axis.horizontal,
+            runSpacing: 12,
+            children: [
+              FilledButton.icon(
+                style: FilledButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                onPressed: () => Navigator.pushNamed(context, '/game/new'),
+                icon: const Icon(Icons.add),
+                label: const Text("Neues Spiel"),
               ),
-              onPressed: () => Navigator.pushNamed(context, '/game/new'),
-              icon: const Icon(Icons.add),
-              label: const Text("Neues Spiel"),
-            )
-          ],
-        )),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                onPressed: () => Navigator.pushNamed(context, '/game'),
+                icon: const Icon(Icons.send),
+                label: const Text("Letztes Spiel weiterführen"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
